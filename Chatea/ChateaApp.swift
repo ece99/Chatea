19b3eef58e6f01f6cd287ae2ed569d34b0482d9f
ece99 <eceserver.ayvaz@gmail.com>
2021-10-20 14:35:29 +0300

    Initial Commit

---
+++ Chatea/ChateaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChateaApp.swift
+//  Chatea
+//
+//  Created by Ece Ayvaz on 20.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ChateaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
