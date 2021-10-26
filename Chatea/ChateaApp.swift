dd63ff5a40dc9534931c7ec60a9de52c769f0214
ece99 <eceserver.ayvaz@gmail.com>
2021-10-26 11:09:38 +0300

    setup firebase

--- Chatea/ChateaApp.swift
+++ Chatea/ChateaApp.swift
@@ -6,9 +6,13 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ChateaApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             MainView()
